0734cc18d245e9c55ce894847454b7ed6ab4bb82
Carl Montplaisir <carl.montplaisir@beslogic.com>
2021-05-26 11:55:47 -0400

    Split registering repo extensions

--- Persistence/Infra.Persistence.EntityFramework.Extensions/AddEfRepositoryExtensions.cs
+++ Persistence/Infra.Persistence.EntityFramework.Extensions/AddEfRepositoryExtensions.cs
@@ -9,60 +9,67 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Infra.Persistence.EntityFramework.Extensions
 {
-    public static class AddEfRepositoryExtensions
+  public static class AddEfRepositoryExtensions
+  {
+    public static IServiceCollection AddEfReadRepository<TDbContext>(
+      this IServiceCollection serviceCollection)
+      where TDbContext : DbContext
     {
-        public static IServiceCollection AddEfRepository<TDbContext>(
-            this IServiceCollection serviceCollection)
-            where TDbContext : DbContext
-        {
-            RegisterRepo<TDbContext>(
-                serviceCollection,
-                typeof(BaseEntity),
-                typeof(IReadRepository<>),
-                typeof(EfReadRepository<,>));
+      RegisterRepo<TDbContext>(
+        serviceCollection,
+        typeof(BaseEntity),
+        typeof(IReadRepository<>),
+        typeof(EfReadRepository<,>));
 
-            RegisterRepo<TDbContext>(
-                serviceCollection,
-                typeof(BaseEntity),
-                typeof(IWriteRepository<>),
-                typeof(EfWriteRepository<,>));
-
-            return serviceCollection;
-        }
-
-        private static void RegisterRepo<TDbContext>(
-            IServiceCollection services,
-            Type baseEntityType,
-            Type repoInterfaceType,
-            Type repoConcreteType)
-            where TDbContext : DbContext
-        {
-            var allAssembly = Assembly.GetAssembly(typeof(TDbContext))
-                ?.GetReferencedAssemblies();
-
-            if (allAssembly == null) return;
-
-            foreach (var assemblyName in allAssembly)
-            {
-                var assembly = Assembly.Load(assemblyName);
-
-                foreach (var type in assembly.GetTypes()
-                    .Where(
-                        t => t.IsClass
-                             && !t.IsAbstract
-                             && t.IsAssignableTo(baseEntityType)
-                             && t.IsAssignableTo(typeof(IRepoQueryable))))
-                {
-                    var interfaceType = repoInterfaceType.MakeGenericType(type);
-                    var concreteType = repoConcreteType.MakeGenericType(
-                        type,
-                        typeof(TDbContext));
-
-                    services.AddTransient(
-                        interfaceType,
-                        concreteType);
-                }
-            }
-        }
+      return serviceCollection;
     }
+
+    public static IServiceCollection AddEfWriteRepository<TDbContext>(
+      this IServiceCollection serviceCollection)
+      where TDbContext : DbContext
+    {
+      RegisterRepo<TDbContext>(
+        serviceCollection,
+        typeof(BaseEntity),
+        typeof(IWriteRepository<>),
+        typeof(EfWriteRepository<,>));
+
+      return serviceCollection;
+    }
+
+    private static void RegisterRepo<TDbContext>(
+      IServiceCollection services,
+      Type baseEntityType,
+      Type repoInterfaceType,
+      Type repoConcreteType)
+      where TDbContext : DbContext
+    {
+      var allAssembly = Assembly.GetAssembly(typeof(TDbContext))
+        ?.GetReferencedAssemblies();
+
+      if (allAssembly == null) return;
+
+      foreach (var assemblyName in allAssembly)
+      {
+        var assembly = Assembly.Load(assemblyName);
+
+        foreach (var type in assembly.GetTypes()
+          .Where(
+            t => t.IsClass
+                 && !t.IsAbstract
+                 && t.IsAssignableTo(baseEntityType)
+                 && t.IsAssignableTo(typeof(IRepoQueryable))))
+        {
+          var interfaceType = repoInterfaceType.MakeGenericType(type);
+          var concreteType = repoConcreteType.MakeGenericType(
+            type,
+            typeof(TDbContext));
+
+          services.AddTransient(
+            interfaceType,
+            concreteType);
+        }
+      }
+    }
+  }
 }
