f37de9fa47094030357363e4a74eb28430088f2c
MrBamboo <nicolas@bamboosoft.ca>
2021-10-20 22:29:16 -0500

    refactor: Change IOAuthSettings to support only one issuer for now

--- Infra.Authentication.Service/Infra.OAuthService/IOAuthSettings.cs
+++ Infra.Authentication.Service/Infra.OAuthService/IOAuthSettings.cs
@@ -1,20 +1,20 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace Infra.oAuthService
 {
-    public interface IOAuthSettings
-    {
-        string[] Audiences { get; }
-        string AuthHeaderName { get; }
+  public interface IOAuthSettings
+  {
+    string[] Audiences { get; }
+    string AuthHeaderName { get; }
 
-        string AuthorizationEndpoint { get; }
+    string AuthorizationEndpoint { get; }
 
-        //string BasicAuthBase64 { get; }
-        string ClientId { get; }
-        string ClientSecret { get; }
-        string Issuer { get; }
-        string Scheme { get; }
-        IDictionary<string, string> Scopes { get; }
-        string TokenUrl { get; }
-    }
+    //string BasicAuthBase64 { get; }
+    string ClientId { get; }
+    string ClientSecret { get; }
+    string Issuer { get; }
+    string Scheme { get; }
+    IDictionary<string, string> Scopes { get; }
+    string TokenUrl { get; }
+  }
 }
--- Infra.Authentication.Service/Infra.OAuthService/OAuthSettings.cs
+++ Infra.Authentication.Service/Infra.OAuthService/OAuthSettings.cs
@@ -1,25 +1,24 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Infra.oAuthService
 {
-    public class OAuthSettings : IOAuthSettings
-    {
-        public string TokenUrl { get; set; }
-        public string ClientId { get; set; }
-        public string ClientSecret { get; set; }
-        public string[] Audiences { get; set; }
-        public string Issuer { get; set; }
-        public string Scheme { get; set; }
+  public class OAuthSettings : IOAuthSettings
+  {
+    public string TokenUrl { get; set; }
+    public string ClientId { get; set; }
+    public string ClientSecret { get; set; }
+    public string[] Audiences { get; set; }
+    public string Issuer { get; set; }
+    public string Scheme { get; set; }
 
-        public string AuthHeaderName { get; set; }
+    public string AuthHeaderName { get; set; }
 
-        public IDictionary<string, string> Scopes => new Dictionary<string, string>()
+    public IDictionary<string, string> Scopes => new Dictionary<string, string>()
         {
             {"custom_scope", "custom scope for CC defined in OKTA"},
         };
 
-        public string AuthorizationEndpoint { get; set; }
-
-        //public string BasicAuthBase64 => throw new System.NotImplementedException();
-    }
+    public string AuthorizationEndpoint { get; set; }
+  }
 }
