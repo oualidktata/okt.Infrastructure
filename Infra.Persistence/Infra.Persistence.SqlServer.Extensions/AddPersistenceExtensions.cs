b14acdcb7c40d1f426af86c3a96ceb25080a64a2
Nicolas Charette-Naud <nicolas.charette-naud@beslogic.com>
2021-11-03 14:53:44 -0400

    refactor: Add overload to AddPersistenceExtensions to support for using Assembly directly

--- Infra.Persistence/Infra.Persistence.SqlServer.Extensions/AddPersistenceExtensions.cs
+++ Infra.Persistence/Infra.Persistence.SqlServer.Extensions/AddPersistenceExtensions.cs
@@ -1,32 +1,41 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.Reflection;
 
 namespace Infra.Persistence.SqlServer.Extensions
 {
-    public static class AddPersistenceExtensions
+  public static class AddPersistenceExtensions
+  {
+    public static IServiceCollection AddSqlServerPersistence<TDbContext>(
+        this IServiceCollection services,
+        IConfiguration configuration,
+        string connectionStringName,
+        Assembly runningAssembly)
+        where TDbContext : DbContext
     {
-        public static IServiceCollection AddSqlServerPersistence<TDbContext>(
-            this IServiceCollection services,
-            IConfiguration configuration,
-            string connectionStringName,
-            string runningAssembly)
-            where TDbContext : DbContext
-        {
-            var connectionString = configuration.GetConnectionString(connectionStringName);
-            services.AddPooledDbContextFactory<TDbContext>(
-                (
-                        serviceProvider,
-                        opt) =>
-                    opt
-                        .UseLazyLoadingProxies()
-                        .UseSqlServer(
-                            connectionString,
-                            b => b.MigrationsAssembly(runningAssembly))
-                        .UseLoggerFactory(serviceProvider.GetRequiredService<ILoggerFactory>()));
-
-            return services;
-        }
+      var runningAssemblyName = runningAssembly.GetName().Name;
+      return services.AddSqlServerPersistence<TDbContext>(configuration, connectionStringName, runningAssemblyName);
     }
+
+    public static IServiceCollection AddSqlServerPersistence<TDbContext>(
+        this IServiceCollection services,
+        IConfiguration configuration,
+        string connectionStringName,
+        string runningAssembly)
+        where TDbContext : DbContext
+    {
+      var connectionString = configuration.GetConnectionString(connectionStringName);
+      services.AddPooledDbContextFactory<TDbContext>((sp, opt) =>
+      {
+        var loggerFactory = sp.GetRequiredService<ILoggerFactory>();
+        opt.UseLazyLoadingProxies()
+           .UseSqlServer(connectionString, b => b.MigrationsAssembly(runningAssembly))
+           .UseLoggerFactory(loggerFactory);
+      });
+
+      return services;
+    }
+  }
 }
